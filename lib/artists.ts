02a7a26bd1dd12cd1487d94f64eeef69676004bf
삼 일 (samilimik) <seong.hyexn@gmail.com>
2025-12-23 10:53:05 +0900

    Create artists.ts

---
+++ lib/artists.ts
@@ -0,0 +1,23 @@
+import { Artist } from "./types";
+
+export const artists: Artist[] = [
+  {
+    name: "레오나르도 다빈치",
+    image: "/artists/davinci.jpg",
+    period: "르네상스",
+    title: "모나리자",
+  },
+  {
+    name: "빈센트 반 고흐",
+    image: "/artists/gogh.jpg",
+    period: "후기 인상주의",
+    title: "별이 빛나는 밤",
+  },
+  {
+    name: "클로드 모네",
+    image: "/artists/monet.jpg",
+    period: "인상주의",
+    title: "수련",
+  },
+  // ...
+];

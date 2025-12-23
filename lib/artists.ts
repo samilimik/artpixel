01ed5a409d07b970ac4c0f322b04dbb83bc32fac
삼 일 (samilimik) <seong.hyexn@gmail.com>
2025-12-23 11:04:43 +0900

    Update artists.ts

--- lib/artists.ts
+++ lib/artists.ts
@@ -1,6 +1,11 @@
 import { Artist } from "./types";
 
 export const artists: Artist[] = [
+  // 작성시 안내
+  // 이미지 파일은 public 폴더에 있는 artists 폴더에 저장하시면 됩니다.
+  // 이때 이미지 파일의 경로는 /artists/???.jpg 입니다.
+  // 시대가 정확하지 않거나 작가의 이름이 정확하지 않은 그림은 작성을 자제해주세요.
+  
   {
     name: "레오나르도 다빈치",
     image: "/artists/davinci.jpg",
@@ -19,5 +24,5 @@ export const artists: Artist[] = [
     period: "인상주의",
     title: "수련",
   },
-  // ...
+  // 이 아래에 이어서 입력해주세요.
 ];
